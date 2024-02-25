855bfd41e7d9c175144bd36f9c04d747548c624a
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2024-02-25 10:52:37 +0700

    add HealthCheckHandler

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jirawan-chuapradit/cards_assignment/handler"
 )
 
 func main() {
 	fmt.Println("Hello")
 
+	r := gin.Default()
+
+	r.GET("/ping", handler.HealthCheckHandler)
+	r.Run()
 }
