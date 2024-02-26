eee66e13420c15adc0c3ae736dfdbaab80d7e254
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2024-02-26 13:50:18 +0700

    call cardsRepository

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 
+	"github.com/jirawan-chuapradit/cards_assignment/db"
 	"github.com/jirawan-chuapradit/cards_assignment/router"
 )
 
 func main() {
-	fmt.Println("Hello")
 
-	r := router.Setup()
+	fmt.Println("Hello")
+	conn := db.Setup()
+	defer func() {
+
+		// Disconnect from MongoDB
+		err := conn.Disconnect(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Disconnected from MongoDB.")
+	}()
+	r := router.Setup(conn)
+
 	r.Run()
 }
--- router/router.go
+++ router/router.go
@@ -3,9 +3,12 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jirawan-chuapradit/cards_assignment/handler"
+	"github.com/jirawan-chuapradit/cards_assignment/repository"
+	"github.com/jirawan-chuapradit/cards_assignment/service"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func Setup() *gin.Engine {
+func Setup(conn *mongo.Client) *gin.Engine {
 
 	r := gin.Default()
 
@@ -14,8 +17,14 @@ func Setup() *gin.Engine {
 	baseRouter := r.Group("/api")
 	cardsRouter := baseRouter.Group("/cards")
 
-	_ = cardsRouter
-	cardHandler := handler.NewCardsHandler()
+	// repository
+	cardsRepository := repository.NewCardsRepository(conn)
+
+	// service
+	cardsServ := service.NewCardsService(cardsRepository)
+
+	// handler
+	cardHandler := handler.NewCardsHandler(cardsServ)
 	cardsRouter.GET("/:cardId", cardHandler.FindById)
 	return r
 }
