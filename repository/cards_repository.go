2b373d725769cb6e002c89d058e741c31216355f
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2024-02-26 20:44:47 +0700

    update card repo

--- repository/cards_repository.go
+++ repository/cards_repository.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/jirawan-chuapradit/cards_assignment/models"
+	"github.com/jirawan-chuapradit/cards_assignment/models/request"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -13,6 +14,7 @@ type CardsRepository interface {
 	FindById(ctx context.Context, cardsId primitive.ObjectID) (cardsDetails models.CardsDetail, err error)
 	FindAll(ctx context.Context) ([]models.CardsDetail, error)
 	Create(ctx context.Context, card models.CardsDetail) (models.CardsDetail, error)
+	Update(ctx context.Context, cardReq request.UpdateCardRequestBody) error
 }
 
 type cardsRepository struct {
@@ -58,4 +60,16 @@ func (r *cardsRepository) Create(ctx context.Context, card models.CardsDetail) (
 
 	card.ID = result.InsertedID.(primitive.ObjectID)
 	return card, nil
+}
+
+// update card
+func (r *cardsRepository) Update(ctx context.Context, cardReq request.UpdateCardRequestBody) error {
+	filter := map[string]interface{}{
+		"_id": cardReq.ID,
+	}
+	update := map[string]interface{}{
+		"$set": cardReq,
+	}
+
+	return r.CardsAssignmentDatabase.Collection("cards").FindOneAndUpdate(ctx, filter, update).Err()
 }
